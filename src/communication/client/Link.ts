fe92c0b35dffcd411a28fc9ed6468cb1fe8b9cc0
Joao Franco <joao.franco@jsfsi.com>
2020-07-19 13:20:43 +0100

    Add constructor to link for easier initialization

--- src/communication/client/Link.ts
+++ src/communication/client/Link.ts
@@ -10,6 +10,17 @@ export class Link<T = { [key: string]: string }> {
     isUrlTemplate?: boolean
     authRequired?: boolean
     template?: T
+
+    constructor(link: Link<T>) {
+        this.rel = link.rel
+        this.target = link.target
+        this.method = link.method
+        this.href = link.href
+        this.headers = link.headers
+        this.isUrlTemplate = link.isUrlTemplate
+        this.authRequired = link.authRequired
+        this.template = link.template
+    }
 }
 
 export class SimpleHateoasRule extends Link {}
