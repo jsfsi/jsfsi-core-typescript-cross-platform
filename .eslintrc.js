44b62955996561be8c2601c685ebd97484cc763f
Joao Franco <joao.franco@jsfsi.com>
2021-04-19 14:37:02 +0100

    Update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,11 @@
 // Note: I prefer using a JavaScript file for the .eslintrc file (instead of a JSON file)
 // as it supports comments that can be used to better describe rules.
 module.exports = {
+  ignorePatterns: ["*.d.ts"],
   parser: '@typescript-eslint/parser',  // Specifies the ESLint parser
   extends: [
     'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    'prettier/@typescript-eslint',  // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'prettier',  // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     'plugin:prettier/recommended',  // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
     'plugin:security-node/recommended',
   ],
