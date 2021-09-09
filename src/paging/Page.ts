10a31ecf9df8a8eea822a6f5d3d6e509393f048c
Joao Franco <joao.franco@jsfsi.com>
2021-09-09 22:06:18 +0100

    Convert pages interfaces to classes

--- src/paging/Page.ts
+++ src/paging/Page.ts
@@ -1,9 +1,18 @@
 // Required to avoid importing page with reference to server side libraries in client side. Useful to mock paged requests
-export interface Page<T> {
+export class Page<T> {
   pages: number
   nextPage: number
   totalElements: number
   currentPage: number
   pageSize: number
   elements: Array<T>
+
+  constructor(page?: Page<T>) {
+    this.pages = page?.pages || 0
+    this.nextPage = page?.nextPage || 0
+    this.totalElements = page?.totalElements || 0
+    this.currentPage = page?.currentPage || 0
+    this.pageSize = page?.pageSize || 0
+    this.elements = page?.elements || []
+  }
 }
--- src/paging/PageQuery.ts
+++ src/paging/PageQuery.ts
@@ -1,4 +1,9 @@
-export interface PageQuery {
+export class PageQuery {
   page: number
   pageSize: number
+
+  constructor(query?: PageQuery) {
+    this.page = query?.page || 0
+    this.pageSize = query?.pageSize || 0
+  }
 }
