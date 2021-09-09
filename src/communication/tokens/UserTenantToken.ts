ced09d6c5fc115c91f8ed98281794fc5077c2098
Joao Franco <joao.franco@jsfsi.com>
2021-09-09 21:59:46 +0100

    UserTenantToken must extend records for nodejs proper usage

--- src/communication/tokens/UserTenantToken.ts
+++ src/communication/tokens/UserTenantToken.ts
@@ -1,6 +1,8 @@
 import { TenantToken } from './TenantToken'
 
-export class UserTenantToken {
+export class UserTenantToken implements Record<string, unknown> {
+  [x: string]: unknown
+
   exp?: number
   iat?: number
   id: string
